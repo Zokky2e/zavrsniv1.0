9acd743d0b0e8454ba030050e7e68f7e3bd003f8
Zokky2e <icro4.gaming@hotmail.com>
2022-04-24 18:14:59 +0200

    zavrsniv1.8

--- src/pages/MyNotes.js
+++ src/pages/MyNotes.js
@@ -3,34 +3,34 @@ import { Calendar } from "react-calendar";
 import { useAuthValue } from "../components/User/UserContext";
 import "react-calendar/dist/Calendar.css";
 import { db } from "../firebase";
-import { get, ref, set } from "firebase/database";
+import { get, onValue, ref, set } from "firebase/database";
 import NoteList from "../components/Notes/NoteList";
 function MyNotes() {
   const [value, onChange] = useState(new Date());
-
   const [loadedNotes, setLoadedNotes] = useState([]);
   const currentUser = useAuthValue();
 
   useEffect(() => {
     if (currentUser !== null) {
-      const notes = ["Click me to edit!"];
+      let notes = ["Click me to edit!"];
       const dbRef = ref(
         db,
         currentUser.uid + "/" + value.toDateString() + "/notes"
       );
-      get(dbRef).then((snapshot) => {
+      onValue((dbRef), ((snapshot) => {
         if (snapshot.exists()) {
+          notes = [];
+          setLoadedNotes([]);
           snapshot.forEach((childSnapshot) => {
             const note = {
               key: childSnapshot.key.toString(),
               childData: childSnapshot.val(),
             };
             notes.push(note);
-            console.log(note.key);
           });
           setLoadedNotes(notes);
         } else set(dbRef, notes);
-      });
+      }));
     }
   }, [currentUser, value]);
   if (currentUser === null) {
