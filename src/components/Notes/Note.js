6b145f0cc2c0a8298b2a4987d049952ce864e792
Zokky2e <icro4.gaming@hotmail.com>
2022-04-28 11:58:52 +0200

    zavrsniv1.8.4

--- src/components/Notes/Note.js
+++ src/components/Notes/Note.js
@@ -1,4 +1,4 @@
-import { ref, remove } from "firebase/database";
+import { ref, remove, set} from "firebase/database";
 import React, { useState } from "react";
 import { db } from "../../firebase";
 import Frame from "../UI/Frame";
@@ -8,30 +8,77 @@ import classes from "./Note.module.css";
 function Note(props) {
   const [content, changeContent] = useState(null);
   const currentUser = useAuthValue();
+  const [textInput, setTextInput] = useState("");
 
   function onDeleteHandler() {
+    changeContent(null);
     const dbRef = ref(
       db,
       currentUser.uid + "/" + props.date + "/notes/" + props?.kljuc
     );
     remove(dbRef).then(console.log("deleted key: " + props?.kljuc));
   }
+
+  function onConfirmEditHandler() {
+    console.log("i entered here!!");
+    const dbRef = ref(
+      db,
+      currentUser.uid + "/" + props.date + "/notes/" + props?.kljuc
+    );
+    const enteredText = textInput.current.value;
+    set(dbRef, enteredText).then(console.log("upated note: " + enteredText));
+    changeContent(null);
+  }
+  function onCancelEditHandler() {
+    changeContent(null)
+  }
+
+  function onChangeHandler(e){
+    setTextInput(e.target.value);
+  }
+
   function onEditHandler() {
-    //TODO
+    setTextInput(props?.data)
+    changeContent(
+      <div >
+        <input
+          className={classes.newNote}
+          required
+          rows="2"
+          id="text"
+          type="text"
+          value={textInput}
+          onInput={onChangeHandler(this.value)}
+        />
+        <button className={classes.button} onClick={onConfirmEditHandler}>
+          &#10004;
+        </button>
+        <button
+          style={{ fontWeight: "bold" }}
+          className={classes.button}
+          onClick={onCancelEditHandler}
+        >
+          &#10005;
+        </button>
+      </div>
+    );
   }
 
   function onClickHandler() {
-    if(content){
-      changeContent(null)
-    }else{
+    if (content) {
+      changeContent(null);
+    } else {
       changeContent(
         <div style={{ float: "right" }}>
-          <button className={classes.button} onClick={onEditHandler}>Edit</button>
-          <button className={classes.button} onClick={onDeleteHandler}>Delete</button>
+          <button className={classes.button} onClick={onEditHandler}>
+            Edit
+          </button>
+          <button className={classes.button} onClick={onDeleteHandler}>
+            Delete
+          </button>
         </div>
       );
     }
-    
   }
 
   return (
@@ -40,7 +87,6 @@ function Note(props) {
         <span onClick={onClickHandler}>{props.data}</span>
         {content}
       </li>
-      
     </Frame>
   );
 }
